73ce6c561e08a846320efc1899177ffee9e4b3aa
nishs9 <snishk@gmail.com>
2020-08-21 14:29:17 -0400

    Finished the template for the team page

--- nextjs-blog/pages/posts/testData.js
+++ nextjs-blog/pages/posts/testData.js
@@ -36,6 +36,30 @@ export default function Test() {
                     />
                 </div>
             </section>
+            <div className= {styles.row} />
+            <section className={`${utilStyles.headingTeamPage} ${utilStyles.padding1px}`}>
+                <p>
+                    <i>2019-2020 Season Penalty Report</i>
+                </p>
+                <CsvToHtmlTable data={csvData}
+                csvDelimiter="," 
+                tableClassName = {utilStyles.table}
+                tableRowClassName = {utilStyles.tr} />
+                <div class={styles.column}>
+                    <p><i>EPC boxplot</i></p>
+                    <img
+                    src="/images/ARI_allExpPointsboxplot.png"
+                    className={styles.graphImage}
+                    />
+                </div>
+                <div class={styles.column}>
+                    <p><i>EPC for each game</i></p>
+                    <img
+                    src="/images/ARI_allExpPointsgraph.png"
+                    className={styles.graphImage}
+                    />
+                </div>
+            </section>
         </Layout>
     )
 }
